b3ecd62d73745da1bfc5c10994ee065cbeef7928
smessina <silvio.messina@eant.tech>
2020-01-31 16:04:51 -0300

    Bases para una API REST

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+- API RESTFUL: PELICULAS
+
+http://localhost/api/peliculas
+
+- GET: (/ o /14) Obtener todas o una pelicula puntual
+- POST: (/) Recibe y genera una nueva pelicula
+- PUT: (/14) Recibe y actualiza una pelicula puntual
+- DELETE: (/14) Recibe y borra una pelicula puntual
+*/
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const api = express()
+
+api.listen(80)
+
+//Endpoints
+api.get("/api/peliculas", function(request, response){
+	response.end("Aca voy a mostrar peliculas")
+})
+
+api.post("/api/peliculas", function(request, response){
+	response.end("Aca voy a crear peliculas")
+})
+
+api.put("/api/peliculas", function(request, response){
+	response.end("Aca voy a actualizar peliculas")
+})
+
+api.delete("/api/peliculas", function(request, response){
+	response.end("Aca voy a borrar peliculas")
+})
